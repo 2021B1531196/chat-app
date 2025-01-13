55bdb56e651f0adbe946c9d78d9b161d37fce6e1
siyaar <shubham.21b1531196@abes.ac.in>
2025-01-13 15:12:40 +0530

    added card componets insdide the feed.jsx also implemented its css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import "../pages/Home.css"
 import { Sidebar } from '../components/Sidebar'
+import Feed from '../components/Feed'
 
 export const Home = ({sidebar}) => {
   return (
     <>
         <Sidebar sidebar={sidebar}/>
+        <div className={`container ${sidebar?"":"larfe-conatiner"}`}>
+          <Feed/>
+        </div>
     
     </>
   )
